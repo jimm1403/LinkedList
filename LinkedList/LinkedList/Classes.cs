2259762e39d7f513091c5365f8bbe830fb8656a3
jimmi <jimmi@DESKTOP-2OTI0T5>
2017-02-28 12:37:22 +0100

    test

---
+++ LinkedList/LinkedList/Classes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinkedList
+{
+    class MyList
+    {
+        string test;
+        public Node Head { get; set; }
+        public string NodeToString()
+        {
+            Node tempNode = Head;
+            while (tempNode != null)
+            {
+                Console.WriteLine(tempNode.Data.ToString());
+                tempNode = tempNode.Next;
+            }
+            return "This index does not exits.";
+        }
+        public void Insert(ClubMember clubMember)
+        {
+            Node newNode = new Node() { Next = Head, Data = clubMember };
+            Head = newNode;
+        }
+        public void Delete()
+        {
+            Head = Head.Next;
+        }
+        public Node Search(int index)
+        {
+            Node tempNode = Head;
+            if (tempNode == null)
+            {
+                Console.WriteLine("Det findes ikke.");
+            }
+            else
+            {
+                for (int i = 1; i < index; i++)
+                {
+                    tempNode = tempNode.Next;
+                }
+            }
+            return tempNode;
+        }
+    }
+    class Node
+    {
+        public Node Next { get; set; }
+        public ClubMember Data { get; set; }
+    }
+    class ClubMember
+    {
+        public int Nr { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return Nr.ToString() + " " + Fname + " " + Lname + " " + Age.ToString();
+        }
+    }
+
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        MyList myList = new MyList();
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+            myProgram.Search();
+            
+        }
+        public void Run()
+        {
+            
+            ClubMember newMember1 = new ClubMember() { Nr = 1, Fname = "Jimmi", Lname = "Christensen", Age = 24 };
+            ClubMember newMember2 = new ClubMember() { Nr = 2, Fname = "Lars", Lname = "Christensen", Age = 23 };
+            ClubMember newMember3 = new ClubMember() { Nr = 3, Fname = "Hans", Lname = "Christensen", Age = 22 };
+            ClubMember newMember4 = new ClubMember() { Nr = 4, Fname = "Katrine", Lname = "Christensen", Age = 21 };
+            //Console.WriteLine("Add new member, with a nr, first name, lastname and age.");
+            //ClubMember newMember = new ClubMember();
+            //Console.WriteLine("Nr.");
+            //newMember.Nr = int.Parse(Console.ReadLine());
+            //Console.WriteLine("First name.");
+            //newMember.Fname = Console.ReadLine();
+            //Console.WriteLine("Last name.");
+            //newMember.Lname = Console.ReadLine();
+            //Console.WriteLine("Age.");
+            //newMember.Age = int.Parse(Console.ReadLine());
+            myList.Insert(newMember1);
+            myList.Insert(newMember2);
+            myList.Insert(newMember3);
+            myList.Insert(newMember4);
+
+            //myList.NodeToString();
+            //Console.ReadKey();
+
+        }
+        public void Search()
+        {
+            Console.WriteLine("Search by index 1 - 4.");
+            int tempSearchIndex = int.Parse(Console.ReadLine());
+            Console.WriteLine(myList.Search(tempSearchIndex).Data.ToString());
+            Console.ReadKey();
+        }
+    }
+}
+
